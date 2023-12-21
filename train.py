d2ccb05fde1a7f04903d36e3e5a861cd1d4249f7
tt622 <727769788@qq.com>
2023-12-21 08:25:12 -0500

    nofreeze

--- train.py
+++ train.py
@@ -11,6 +11,7 @@ from segment_anything import sam_model_registry
 from segment_anything.utils.transforms import ResizeLongestSide
 import argparse
 from torch import nn
+import cv2
 # set seeds
 torch.manual_seed(2023)
 np.random.seed(2023)
@@ -20,16 +21,23 @@ np.random.seed(2023)
 class NpyDataset(Dataset): 
     def __init__(self, data_root):
         self.data_root = data_root
-        self.gt_path = join(data_root, 'npy_gts')
-        self.embed_path = join(data_root, 'npy_embs')
+        self.gt_path = join(data_root, 'mask')
+        self.embed_path = join(data_root, 'img')
         self.npy_files = sorted(os.listdir(self.gt_path))
     
     def __len__(self):
         return len(self.npy_files)
 
     def __getitem__(self, index):
-        gt2D = np.load(join(self.gt_path, self.npy_files[index]))
-        img_embed = np.load(join(self.embed_path, self.npy_files[index]))
+        # gt2D = np.load(join(self.gt_path, self.npy_files[index]))
+        # img_embed = np.load(join(self.embed_path, self.npy_files[index]))
+        img = cv2.imread(join(self.embed_path, self.npy_files)) # (256, 256, 3)
+        gt2D = cv2.imread(join(self.gt_path, self.npy_files))[:,:,0]
+        resize_img = sam_transform.apply_image(img)
+        resize_img_tensor = torch.as_tensor(resize_img.transpose(2, 0, 1)).to('cuda:0')
+        # model input: (1, 3, 1024, 1024)
+        input_image = sam_model.preprocess(resize_img_tensor[None,:,:,:]) # (1, 3, 1024, 1024)
+
         y_indices, x_indices = np.where(gt2D > 0)
         x_min, x_max = np.min(x_indices), np.max(x_indices)
         y_min, y_max = np.min(y_indices), np.max(y_indices)
@@ -41,11 +49,11 @@ class NpyDataset(Dataset):
         y_max = min(H, y_max + np.random.randint(0, 20))
         bboxes = np.array([x_min, y_min, x_max, y_max])
         # convert img embedding, mask, bounding box to torch tensor
-        return torch.tensor(img_embed).float(), torch.tensor(gt2D[None, :,:]).long(), torch.tensor(bboxes).float()
+        return torch.tensor(input_image).float(), torch.tensor(gt2D[None, :,:]).long(), torch.tensor(bboxes).float()
 
 # %% set up parser
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--tr_npy_path', type=str, default='data/Tr_npy_new', help='path to training npy files; two subfolders: npy_gts and npy_embs')
+parser.add_argument('-i', '--data_root', type=str, default='/Road/deepGlobe/train_split', help='path to training npy files; two subfolders: img and mask')
 parser.add_argument('--task_name', type=str, default='SAM-ViT-B')
 parser.add_argument('--model_type', type=str, default='vit_b')
 parser.add_argument('--checkpoint', type=str, default='work_dir/SAM/sam_vit_b_01ec64.pth')
@@ -53,7 +61,7 @@ parser.add_argument('--device', type=str, default='cuda:0')
 parser.add_argument('--work_dir', type=str, default='work_dir_test')
 # train
 parser.add_argument('--num_epochs', type=int, default=1000)
-parser.add_argument('--batch_size', type=int, default=8)
+parser.add_argument('--batch_size', type=int, default=4)
 parser.add_argument('--lr', type=float, default=1e-5)
 parser.add_argument('--weight_decay', type=float, default=0)
 args = parser.parse_args()
@@ -64,6 +72,7 @@ device = args.device
 model_save_path = join(args.work_dir, args.task_name)
 os.makedirs(model_save_path, exist_ok=True)
 sam_model = sam_model_registry[args.model_type](checkpoint=args.checkpoint).to(device)
+sam_transform = ResizeLongestSide(sam_model.image_encoder.img_size)
 sam_model.train()
 
 mylog = open(model_save_path+"/train.log",'w')
@@ -80,13 +89,13 @@ m = nn.Sigmoid()
 num_epochs = args.num_epochs
 losses = []
 best_loss = 1e10
-train_dataset = NpyDataset(args.tr_npy_path)
+train_dataset = NpyDataset(args.data_root)
 train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, drop_last=True)
 for epoch in range(num_epochs):
     epoch_loss = 0
     # Just train on the first 20 examples
-    for step, (image_embedding, gt2D, boxes) in enumerate(tqdm(train_dataloader)):
-        # do not compute gradients for image encoder and prompt encoder
+    for step, (input_image, gt2D, boxes) in enumerate(tqdm(train_dataloader)):
+        # do not compute gradients for prompt encoder
         with torch.no_grad():
             # convert box to 1024x1024 grid
             box_np = boxes.numpy()
@@ -102,6 +111,8 @@ for epoch in range(num_epochs):
                 boxes=None,
                 masks=None,
             )
+        # do not freeze image encoder
+        image_embedding = sam_model.image_encoder(input_image)
             # print("image_embedding:" + str(image_embedding.shape))
         low_res_masks, iou_predictions = sam_model.mask_decoder(
             image_embeddings=image_embedding.to(device), # (B, 256, 64, 64)
