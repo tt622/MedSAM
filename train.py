85fdc85e8f19055bc90373e63a462b3e3e7692d7
tt622 <727769788@qq.com>
2023-12-23 00:53:36 -0500

    scribble

--- train.py
+++ train.py
@@ -11,6 +11,8 @@ from segment_anything import sam_model_registry
 from segment_anything.utils.transforms import ResizeLongestSide
 import argparse
 from torch import nn
+import cv2
+from skimage import io, segmentation, transform
 # set seeds
 torch.manual_seed(2023)
 np.random.seed(2023)
@@ -20,40 +22,38 @@ np.random.seed(2023)
 class NpyDataset(Dataset): 
     def __init__(self, data_root):
         self.data_root = data_root
-        self.gt_path = join(data_root, 'npy_gts')
-        self.embed_path = join(data_root, 'npy_embs')
-        self.npy_files = sorted(os.listdir(self.gt_path))
+        self.img_path = args.img_path
+        self.scribble_path = args.scribble_path
+        self.gt_path = args.gt_path
+        self.file_name = sorted(os.listdir(self.gt_path))
     
     def __len__(self):
-        return len(self.npy_files)
+        return len(self.file_name)
 
     def __getitem__(self, index):
-        gt2D = np.load(join(self.gt_path, self.npy_files[index]))
-        img_embed = np.load(join(self.embed_path, self.npy_files[index]))
-        y_indices, x_indices = np.where(gt2D > 0)
-        x_min, x_max = np.min(x_indices), np.max(x_indices)
-        y_min, y_max = np.min(y_indices), np.max(y_indices)
-        # add perturbation to bounding box coordinates
-        H, W = gt2D.shape
-        x_min = max(0, x_min - np.random.randint(0, 20))
-        x_max = min(W, x_max + np.random.randint(0, 20))
-        y_min = max(0, y_min - np.random.randint(0, 20))
-        y_max = min(H, y_max + np.random.randint(0, 20))
-        bboxes = np.array([x_min, y_min, x_max, y_max])
+        img = cv2.imread(join(self.img_path, self.file_name[index])) # (1024, 1024, 3)
+        scribble = cv2.imread(join(self.scribble_path, self.file_name[index]))[:,:,0]
+        gt = cv2.imread(join(self.gt_path, self.file_name[index]))[:,:,0]
+        resize_img = sam_transform.apply_image(img)
+        img = torch.as_tensor(resize_img.transpose(2, 0, 1)).to(device)
         # convert img embedding, mask, bounding box to torch tensor
-        return torch.tensor(img_embed).float(), torch.tensor(gt2D[None, :,:]).long(), torch.tensor(bboxes).float()
+        return torch.tensor(img).float(), torch.tensor(scribble[None, :,:]).float(), torch.tensor(gt[None, :,:]).long()
 
 # %% set up parser
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--tr_npy_path', type=str, default='data/Tr_npy_new', help='path to training npy files; two subfolders: npy_gts and npy_embs')
-parser.add_argument('--task_name', type=str, default='SAM-ViT-B')
+# parser.add_argument('-i', '--tr_npy_path', type=str, default='data/Tr_npy_new', help='path to training npy files; two subfolders: npy_gts and npy_embs')
+parser.add_argument('--img_path', type=str, default="/Road/datasets/partial-osm/imagery", help='path to training images')
+parser.add_argument('--scribble_path', type=str, default="/Road/datasets/partial-osm/masks_75", help='path to training scribble prompts')
+parser.add_argument('--gt_path', type=str, default="/Road/datasets/partial-osm/masks", help='path to training gts')
+
+parser.add_argument('--task_name', type=str, default='SAM-B-partialOSM-scribble-BCELoss')
 parser.add_argument('--model_type', type=str, default='vit_b')
 parser.add_argument('--checkpoint', type=str, default='work_dir/SAM/sam_vit_b_01ec64.pth')
 parser.add_argument('--device', type=str, default='cuda:0')
 parser.add_argument('--work_dir', type=str, default='work_dir_test')
 # train
 parser.add_argument('--num_epochs', type=int, default=1000)
-parser.add_argument('--batch_size', type=int, default=8)
+parser.add_argument('--batch_size', type=int, default=4)
 parser.add_argument('--lr', type=float, default=1e-5)
 parser.add_argument('--weight_decay', type=float, default=0)
 args = parser.parse_args()
@@ -64,12 +64,13 @@ device = args.device
 model_save_path = join(args.work_dir, args.task_name)
 os.makedirs(model_save_path, exist_ok=True)
 sam_model = sam_model_registry[args.model_type](checkpoint=args.checkpoint).to(device)
+sam_transform = ResizeLongestSide(sam_model.image_encoder.img_size)
 sam_model.train()
 
 mylog = open(model_save_path+"/train.log",'w')
 
 # Set up the optimizer, hyperparameter tuning will improve performance here
-optimizer = torch.optim.Adam(sam_model.mask_decoder.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+optimizer = torch.optim.Adam(sam_model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 # seg_loss = monai.losses.DiceCELoss(sigmoid=True, squared_pred=True, reduction='mean')
 # mse_Loss=nn.MSELoss()
 bce_Loss = nn.BCELoss()
@@ -80,40 +81,40 @@ m = nn.Sigmoid()
 num_epochs = args.num_epochs
 losses = []
 best_loss = 1e10
-train_dataset = NpyDataset(args.tr_npy_path)
+train_dataset = NpyDataset(args.img_path)
 train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, drop_last=True)
 for epoch in range(num_epochs):
     epoch_loss = 0
     # Just train on the first 20 examples
-    for step, (image_embedding, gt2D, boxes) in enumerate(tqdm(train_dataloader)):
+    for step, (img, scribble, gt) in enumerate(tqdm(train_dataloader)):
         # do not compute gradients for image encoder and prompt encoder
         with torch.no_grad():
-            # convert box to 1024x1024 grid
-            box_np = boxes.numpy()
-            sam_trans = ResizeLongestSide(sam_model.image_encoder.img_size)
-            box = sam_trans.apply_boxes(box_np, (gt2D.shape[-2], gt2D.shape[-1]))
-            box_torch = torch.as_tensor(box, dtype=torch.float, device=device)
-            if len(box_torch.shape) == 2:
-                box_torch = box_torch[:, None, :] # (B, 1, 4)
-            
+            # model input: (B, 3, 1024, 1024)
+            image_embedding = sam_model.image_encoder(img)
             sparse_embeddings, dense_embeddings = sam_model.prompt_encoder(
                 points=None,
-                # boxes=box_torch,
                 boxes=None,
-                masks=None,
+                masks=scribble.to(device),
             )
-            # print("image_embedding:" + str(image_embedding.shape))
+            print("image_embedding:" + str(image_embedding.shape))
+        print("sparse_embeddings:" + str(sparse_embeddings.shape))
+        print("dense_embeddings:" + str(dense_embeddings.shape))
         low_res_masks, iou_predictions = sam_model.mask_decoder(
             image_embeddings=image_embedding.to(device), # (B, 256, 64, 64)
             image_pe=sam_model.prompt_encoder.get_dense_pe(), # (1, 256, 64, 64)
             sparse_prompt_embeddings=sparse_embeddings, # (B, 2, 256)
             dense_prompt_embeddings=dense_embeddings, # (B, 256, 64, 64)
             multimask_output=False,
-          )
+        )
+        masks = sam_model.postprocess_masks(
+            low_res_masks,
+            input_size=img.shape[-2:],
+            original_size=img.shape[-2:],
+        )
         # print(low_res_masks.shape)# (B, 1, 256, 256)
         # loss = seg_loss(low_res_masks, gt2D.to(device))
         # loss = mse_Loss(low_res_masks, gt2D.to(device).float())
-        loss = bce_Loss(m(low_res_masks), m(gt2D).to(device).float())
+        loss = bce_Loss(m(masks), m(gt).to(device).float())
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
